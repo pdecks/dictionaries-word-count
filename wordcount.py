78287c8611bbaad38f240c2d6e474dac414f637a
Hackbright Student <info@hackbrightacademy.com>
2015-09-28 12:56:06 -0700

    Mostly works for twain.txt

--- wordcount.py
+++ wordcount.py
@@ -8,10 +8,13 @@ def produce_word_tokens(filename):
     tokens_list = []
     for line in file_object:
         
+        punc_list = ('!', ',', '.', '--', '?', ':', ';', '"', "'", "_",
+                     "(", ")")
         tokens = line.split()
         for token in tokens:
-            token = token.strip(".!?,;:")
-            tokens_list.append(token)
+            #token = token.strip(".!?,;:")
+            token_out = "".join(char for char in token if char not in punc_list)
+            tokens_list.append(token_out.lower())
         # tokens_list.extend(tokens) 
         # let's ask a question about why this didn't work with extend !!!!
        
@@ -24,15 +27,21 @@ def token_to_dictionary(tokens_list):
     """
     word_dict = {}
     for token in tokens_list:
-        if token in word_dict:
-            word_dict[token] += 1
-        else:
-            word_dict[token] = 1
+    #     if token in word_dict:
+    #         word_dict[token] += 1
+    #     else:
+    #         word_dict[token] = 1
+        word_dict[token] = word_dict.get(token, 0) + 1
 
     return word_dict
 
-
-tokens_list = produce_word_tokens("test.txt")
+file_name = "twain.txt"
+# file_name = "test.txt"
+tokens_list = produce_word_tokens(file_name)
 word_dict = token_to_dictionary(tokens_list)
 
-print word_dict
+# print word_dict
+
+for key in word_dict:
+    word_count = word_dict[key]
+    print "{}: {}".format(key, word_count)
